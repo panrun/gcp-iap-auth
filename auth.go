8ea2b173cc544579363bc7141046410f00935398
Damian Myerscough <Damian.Myerscough@gmail.com>
2019-06-30 22:07:51 -0700

    Move from Glide to Go modules and enable Prometheus metrics for authentication

--- auth.go
+++ auth.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/imkira/gcp-iap-auth/jwt"
+)
+
+var (
+	authRequests = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "gcp_iap_proxy_queries_total",
+		Help: "The total number of queries",
+	})
+	authFailures = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "gcp_iap_proxy_auth_failures_total",
+		Help: "The total number of authentication requests that failed",
+	})
+	authSuccess = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "gcp_iap_proxy_auth_successful_total",
+		Help: "The total number of authentication requests that were successful",
+	})
 )
 
 type userIdentity struct {
@@ -15,6 +32,8 @@ type userIdentity struct {
 }
 
 func authHandler(res http.ResponseWriter, req *http.Request) {
+	authRequests.Inc()
+
 	claims, err := jwt.RequestClaims(req, cfg)
 	if err != nil {
 		if claims == nil || len(claims.Email) == 0 {
@@ -22,6 +41,9 @@ func authHandler(res http.ResponseWriter, req *http.Request) {
 		} else {
 			log.Printf("Failed to authenticate %q (%v)\n", claims.Email, err)
 		}
+
+		authFailures.Inc()
+
 		res.WriteHeader(http.StatusUnauthorized)
 		return
 	}
@@ -33,4 +55,6 @@ func authHandler(res http.ResponseWriter, req *http.Request) {
 	log.Printf("Authenticated %q (token expires at %v)\n", user.Email, expiresAt)
 	res.WriteHeader(http.StatusOK)
 	json.NewEncoder(res).Encode(user)
+
+	authSuccess.Inc()
 }
